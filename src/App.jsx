dc9bff307d63d883b8feebdaba4a4948d87d9d99
rudisiona <rsmageo@gmail.com>
2025-08-28 20:00:29 -0700

    origin fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router';
 
 import NavBar from './components/NavBar/NavBar';
@@ -6,19 +6,46 @@ import SignUpForm from './components/SignUpForm/SignUpForm';
 import SignInForm from './components/SignInForm/SignInForm';
 import Landing from './components/Landing/Landing';
 import Dashboard from './components/Dashboard/Dashboard';
-
+import HootList from './components/HootList/HootList';
 import { UserContext } from './contexts/UserContext';
+import * as hootService from './services/hootService';
+import HootDetails from './components/HootDetails/HootDetails'
 
 const App = () => {
   const { user } = useContext(UserContext);
+  const [hoots, setHoots] = useState([])
+  
+  useEffect(() => {
+    const fetchAllHoots = async () => {
+      const hootsData = await hootService.index();
+  
+      setHoots(hootsData)
+    };
+    if (user) fetchAllHoots();
+  }, [user]); //only run on component mount or user state changes
+  
   
   return (
     <>
       <NavBar/>
       <Routes>
         <Route path='/' element={user ? <Dashboard /> : <Landing />} />
-        <Route path='/sign-up' element={<SignUpForm />} />
-        <Route path='/sign-in' element={<SignInForm />} />
+        {user ? (
+          <>
+            {/* Protected routes (available only to signed-in users) */}
+            <Route path='/hoots' element={<HootList hoots={hoots} />} />
+            <Route 
+              path='/hoots/:hootId'
+              element={<HootDetails />}
+            />  
+          </>
+        ) : (
+          <>
+            {/* Non-user routes (available only to guests) */}
+            <Route path='/sign-up' element={<SignUpForm />} />
+            <Route path='/sign-in' element={<SignInForm />} />
+          </>
+        )}
       </Routes>
     </>
   );
